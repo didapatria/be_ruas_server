4267aa366a5b0ee7f8c41fbd92298045a7ec9f02
didapatria <didapatria3@gmail.com>
2023-06-20 12:18:42 +0700

    Update import keras from tensorflow.keras

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
-from keras.preprocessing.image import ImageDataGenerator
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 import matplotlib.pyplot as plt
 import os
 
